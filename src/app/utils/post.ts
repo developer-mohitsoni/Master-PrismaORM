63d13e7c9c13dc8c04cdb025e9778ca38cc457b2
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 17:38:03 +0530

    Add sample posts data for demonstration purposes

---
+++ src/app/utils/post.ts
@@ -0,0 +1,19 @@
+export const posts = [
+  {
+    title: "GL Bajaj Fraud",
+    content:
+      "GL Bajaj College is Fraud College, They collect from money in Mathura Branch and invest this money in his Noida Branch. And their people are fool they suppose to be a Software Engineer. But it's not true my friend they all are just cheap workers just like anyone working in sales department.",
+    published: true,
+  },
+  {
+    title: "Corruption in Government",
+    content:
+      "Corruption in government is not only in the government itself but also in the people. Government employees are cheap workers who don't give their fair share of money to the people. They just collect money from the people and use it for their own benefit.",
+    published: true,
+  },
+  {
+    title: "Fake Degree from GL Bajaj",
+    content:
+      "They only just gives fake degress to his/her students. And the people live in Noida GL Bajaj Branch. They all are creepy except few ones like mine Friend Pradhumn Kumar Mishra. And Everyone are useless.",
+  },
+];
