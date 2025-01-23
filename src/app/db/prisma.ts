bd82fff3d7f0a4d8420331c4c84e77c949ddb856
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 22:53:06 +0530

    Enhance PrismaClient with @prisma/extension-accelerate for improved performance

--- src/app/db/prisma.ts
+++ src/app/db/prisma.ts
@@ -1,5 +1,6 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { withOptimize } from "@prisma/extension-optimize";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
@@ -8,10 +9,12 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query", "info", "error"],
-  }).$extends(
-    withOptimize({
-      apiKey: process.env.OPTIMIZE_API_KEY as string,
-    })
-  );
+  })
+    .$extends(
+      withOptimize({
+        apiKey: process.env.OPTIMIZE_API_KEY as string,
+      })
+    )
+    .$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
