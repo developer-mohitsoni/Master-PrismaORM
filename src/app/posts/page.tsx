2a4226b3dbc4fca165be5dca5100ba1c03c292bd
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 17:58:35 +0530

    Implement PostPage component to fetch and display posts from the database

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,27 @@
-const HomePage = () => {
+import Link from "next/link";
+import { prisma } from "../db/prisma";
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+}
+
+const PostPage = async () => {
+  const posts = await prisma.post.findMany();
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
       <h1 className="text-3xl font-semibold">All Posts (0)</h1>
 
-      <ul className="border-t border-b border-black/10 py-5 leading-8"></ul>
+      <ul className="border-t border-b border-black/10 py-5 leading-8">
+        {posts.map((post: Post) => (
+          <li key={post.id} className="flex items-center justify-between px-5">
+            <Link href={`posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
 
-export default HomePage;
+export default PostPage;
