3a22a107daba69154ad7f79dbc99bd10b6d317c3
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 22:15:54 +0530

    Add author association to posts with connectOrCreate for default user

--- src/app/utils/post.ts
+++ src/app/utils/post.ts
@@ -1,10 +1,23 @@
-export const posts = [
+import { Prisma } from "@prisma/client";
+
+export const posts: Prisma.PostCreateInput[] = [
   {
     title: "GL Bajaj Fraud",
     slug: "gl-bajaj-fraud",
     content:
       "GL Bajaj College is Fraud College, They collect from money in Mathura Branch and invest this money in his Noida Branch. And their people are fool they suppose to be a Software Engineer. But it's not true my friend they all are just cheap workers just like anyone working in sales department.",
     published: true,
+    author: {
+      connectOrCreate: {
+        where: {
+          email: "john@example.com",
+        },
+        create: {
+          email: "john@example.com",
+          hashedPassword: "chsdbmcnbdsmc531332csdc",
+        },
+      },
+    },
   },
   {
     title: "Corruption in Government",
@@ -12,11 +25,33 @@ export const posts = [
     content:
       "Corruption in government is not only in the government itself but also in the people. Government employees are cheap workers who don't give their fair share of money to the people. They just collect money from the people and use it for their own benefit.",
     published: true,
+    author: {
+      connectOrCreate: {
+        where: {
+          email: "john@example.com",
+        },
+        create: {
+          email: "john@example.com",
+          hashedPassword: "chsdbmcnbdsmc531332csdc",
+        },
+      },
+    },
   },
   {
     title: "Fake Degree from GL Bajaj",
     slug: "fake-degree-from-gl-bajaj",
     content:
       "They only just gives fake degress to his/her students. And the people live in Noida GL Bajaj Branch. They all are creepy except few ones like mine Friend Pradhumn Kumar Mishra. And Everyone are useless.",
+    author: {
+      connectOrCreate: {
+        where: {
+          email: "john@example.com",
+        },
+        create: {
+          email: "john@example.com",
+          hashedPassword: "chsdbmcnbdsmc531332csdc",
+        },
+      },
+    },
   },
 ];
