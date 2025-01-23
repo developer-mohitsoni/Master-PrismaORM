2ca92e70224bc6f92db12e271dd99b58613e3d15
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 19:50:58 +0530

    Add post count display and order posts by creation date in PostPage component and adding pagination

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -9,12 +9,23 @@ const PostPage = async () => {
         contains: "GL",
       },
     },
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+      content: true,
+    },
+    take: 1,
+    skip: 1,
   });
+
+  const postCount = await prisma.post.count();
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-3xl font-semibold">
-        All Posts ({`${posts.length}`})
-      </h1>
+      <h1 className="text-3xl font-semibold">All Posts ({`${postCount}`})</h1>
 
       <ul className="border-t border-b border-black/10 py-5 leading-8">
         {posts.map((post: Post) => (
