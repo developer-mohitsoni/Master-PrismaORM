1117c2ef4ef2ef5972d727f78e01851f21b1ad5e
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 22:16:13 +0530

    Update PostPage to display posts associated with a specific user

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,32 +4,42 @@ import { Post } from "@/types";
 import { createPost } from "@/actions/actions";
 
 const PostPage = async () => {
-  const posts = await prisma.post.findMany({
-    where: {
-      title: {
-        // contains: "GL",
-      },
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-    select: {
-      id: true,
-      title: true,
-      slug: true,
-      content: true,
-    },
-    // take: 1,
-    // skip: 1,
-  });
+  // const posts = await prisma.post.findMany({
+  //   where: {
+  //     title: {
+  //       // contains: "GL",
+  //     },
+  //   },
+  //   orderBy: {
+  //     createdAt: "desc",
+  //   },
+  //   select: {
+  //     id: true,
+  //     title: true,
+  //     slug: true,
+  //     content: true,
+  //   },
+  //   // take: 1,
+  //   // skip: 1,
+  // });
 
-  const postCount = await prisma.post.count();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: "john@example.com",
+    },
+    include: {
+      posts: true,
+    },
+  });
+  // const postCount = await prisma.post.count();
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-3xl font-semibold">All Posts ({`${postCount}`})</h1>
+      <h1 className="text-3xl font-semibold">
+        All Posts ({`${user?.posts.length}`})
+      </h1>
 
       <ul className="border-t border-b border-black/10 py-5 leading-8">
-        {posts.map((post: Post) => (
+        {user?.posts.map((post: Post) => (
           <li key={post.id} className="flex items-center justify-between px-5">
             <Link href={`posts/${post.slug}`}>{post.title}</Link>
           </li>
