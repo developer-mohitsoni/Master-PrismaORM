c802f613b8b279af2fd4e339490cb11bdafa5a9b
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 18:20:23 +0530

    Update post links to use slug for improved URL handling

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -6,12 +6,14 @@ const PostPage = async () => {
   const posts = await prisma.post.findMany();
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-3xl font-semibold">All Posts ({`${posts.length}`})</h1>
+      <h1 className="text-3xl font-semibold">
+        All Posts ({`${posts.length}`})
+      </h1>
 
       <ul className="border-t border-b border-black/10 py-5 leading-8">
         {posts.map((post: Post) => (
           <li key={post.id} className="flex items-center justify-between px-5">
-            <Link href={`posts/${post.id}`}>{post.title}</Link>
+            <Link href={`posts/${post.slug}`}>{post.title}</Link>
           </li>
         ))}
       </ul>
