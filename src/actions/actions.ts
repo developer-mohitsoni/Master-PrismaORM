3d5f00a045e71dd5b34f845884930763fbb1bf28
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-23 20:15:47 +0530

    Add editPost action to update existing posts with new title and content

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -13,3 +13,18 @@ export async function createPost(formData: FormData) {
     },
   });
 }
+
+export async function editPost(formData: FormData, id: string) {
+  await prisma.post.update({
+    where: {
+      id,
+    },
+    data: {
+      title: formData.get("title") as string,
+      content: formData.get("content") as string,
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
+    },
+  });
+}
