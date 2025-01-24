eff03b0bfe20792e9323c40367934ed64022cb75
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-24 13:04:17 +0530

    Add Pulse extension to PrismaClient for enhanced performance monitoring

--- src/app/db/prisma.ts
+++ src/app/db/prisma.ts
@@ -2,7 +2,13 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { withOptimize } from "@prisma/extension-optimize";
+import { withPulse } from "@prisma/extension-pulse/node";
 
 export const prisma = new PrismaClient()
   .$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY as string }))
-  .$extends(withAccelerate());
+  .$extends(withAccelerate())
+  .$extends(
+    withPulse({
+      apiKey: process.env.PULSE_API_KEY as string,
+    })
+  );
