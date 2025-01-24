aaffc71fb55de2b66f8661591445ab4d849953f1
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-24 12:58:03 +0530

    Refactor PrismaClient initialization to use edge version and simplify global handling

--- src/app/db/prisma.ts
+++ src/app/db/prisma.ts
@@ -1,20 +1,8 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { withOptimize } from "@prisma/extension-optimize";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "info", "error"],
-  })
-    .$extends(
-      withOptimize({
-        apiKey: process.env.OPTIMIZE_API_KEY as string,
-      })
-    )
-    .$extends(withAccelerate());
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient()
+  .$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY as string }))
+  .$extends(withAccelerate());
